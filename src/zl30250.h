a638d2d9953431dfafb34148a8f06e8b35528f61
John Simons <jammsimons@gmail.com>
2024-04-27 19:32:05 +0200

    Add zl30250back in and change tslitex to build as SHARED

---
+++ src/zl30250.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: BSD-2-Clause
+ *
+ * This file is part of libtslitex.
+ * Control the ZL30250 in the Thunderscope LiteX design
+ *
+ * Copyright (c) 2024 John Simons <jammsimons@gmail.com>
+ */
+#ifndef _ZL30250_H_
+#define _ZL30250_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
